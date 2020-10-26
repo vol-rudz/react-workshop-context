7626fd39509705749dfd3f24abfba13f57b070a9
YarynaBukovynska <yaryna.bukovynska@gmail.com>
2020-10-26 15:37:05 +0200

    updated date formatting

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -11,7 +11,9 @@ const Sidebar = () => {
   const [date, setDate] = useState(new Date());
   const formattedDay = useMemo(() => {
     const dateObj = new Date(date);
-    return `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`
+    const day = dateObj.getDate() >= 10 ? dateObj.getDate() : `0${dateObj.getDate()}`;
+    const month = dateObj.getMonth() >= 9 ? dateObj.getMonth() + 1 : `0${dateObj.getMonth() + 1}`;
+    return `${dateObj.getFullYear()}-${month}-${day}`
   }, [date]);
   return (
     <aside style={{
